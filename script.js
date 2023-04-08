ee29da6572d3fb7a26d21bc933c709be67661006
Alnisa Putri <81296633+AlnisaPutriCandraKirana@users.noreply.github.com>
2023-04-08 15:19:41 +0700

    first commit

--- script.js
+++ script.js
@@ -1,118 +1,103 @@
-//screen
 const calculatorScreen = document.querySelector('.calculator-screen')
 
 const updateScreen = (number) => {
-    calculatorScreen.value = number
+	calculatorScreen.value = number
 }
 
-//button
 const numbers = document.querySelectorAll(".number")
 
 numbers.forEach((number) => {
-    number.addEventListener("click", (event) => {
-        inputNumber(event.target.value)
-        updateScreen(currentNumber)
-    })
+	number.addEventListener('click', (event) => {
+		inputNumber(event.target.value)
+		updateScreen(currentNumber)
+	})
 })
+
 
 let prevNumber = ''
 let calculationOperator = ''
 let currentNumber = '0'
 
-//membuat function inputNumber
 const inputNumber = (number) => {
-    if (currentNumber === '0') {
-        currentNumber = number
-    } else {
-        currentNumber += number
-    }
+	if (currentNumber === '0') {
+		currentNumber = number
+	} else { 
+		currentNumber += number
+	}
 }
 
-//operator
+
 const operators = document.querySelectorAll(".operator")
 
 operators.forEach((operator) => {
-    operator.addEventListener("click", (event) => {
-        inputOperator(event.target.value)
-    })
+	operator.addEventListener('click', (event) => {
+		inputOperator(event.target.value)
+	})
 })
 
-//function operator
 const inputOperator = (operator) => {
-    if (calculationOperator === '') {
-        prevNumber = currentNumber
-    }
-    calculationOperator = operator
-    currentNumber = '0'
+	if (calculationOperator === '') {
+	prevNumber = currentNumber	
+	}
+	calculationOperator = operator
+	currentNumber = '0'
 }
 
-//equal atau sama dengan
 const equalSign = document.querySelector('.equal-sign')
 
 equalSign.addEventListener('click', () => {
-    calculate()
-    updateScreen(currentNumber)
+	calculate()
+	updateScreen(currentNumber)
 })
 
-//function calculate
 const calculate = () => {
-    let result = ''
-    switch (calculationOperator) {
-        case "+":
-            result = parseFloat(prevNumber) + parseFloat(currentNumber)
-            break
-
-        case "-":
-            result = prevNumber - currentNumber
-            break
-
-        case "*":
-            result = prevNumber * currentNumber
-            break
-
-        case "/":
-            result = prevNumber / currentNumber
-            break
-
-        case "%":
-            result = parseFloat(prevNumber) / 100
-            break
-
-        default:
-            return
-    }
-
-    currentNumber = result
-    calculationOperator = ''
+	let result = ''
+	switch(calculationOperator) {
+		case "+":
+			result = parseFloat(prevNumber) + parseFloat(currentNumber)
+		break
+		case "-":
+			result = prevNumber - currentNumber
+		break
+		case "*":
+			result = prevNumber * currentNumber
+		break
+		case "/":
+			result = prevNumber / currentNumber
+		break
+		case "%":
+			result = parseFloat(prevNumber) / 100
+		break
+		default:
+		return
+	}
+	currentNumber = result
+	calculationOperator = ''
 }
 
-//button AC
 const clearBtn = document.querySelector('.all-clear')
 
-//function AC
 const clearAll = () => {
-    prevNumber = ''
-    calculationOperator = ''
-    currentNumber = '0'
+	prevNumber = '' 
+	calculationOperator = ''
+	currentNumber = '0' 
 }
 
 clearBtn.addEventListener('click', () => {
-    clearAll()
-    updateScreen(currentNumber)
+	clearAll();
+	updateScreen(currentNumber)
 })
 
-//button decimal
 const decimal = document.querySelector('.decimal')
 
-decimal.addEventListener('click', (event) => {
-    inputDecimal(event.target.value)
-    updateScreen(currentNumber)
+decimal.addEventListener('click', (event) =>{
+	inputDecimal(event.target.value)
+	updateScreen(currentNumber)
 })
 
-//function decimal
-inputDecimal = (dot) => {
-    if (currentNumber.includes('.')) {
-        return
-    }
-    currentNumber += dot
+inputDecimal = (dot) =>{
+	if (currentNumber.includes('.')) {
+		return
+	}
+	currentNumber += dot
 }
